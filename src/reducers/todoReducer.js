bbee6d72f75d0f046fd9246e5a70969d3d5267ed
tcmorrison56 <155089594+tcmorrison56@users.noreply.github.com>
2026-09-05 18:20:44 -0700

    complete reducer code

--- src/reducers/todoReducer.js
+++ src/reducers/todoReducer.js
@@ -9,9 +9,15 @@ export const TODO_ACTIONS = {
   ADD_TODO_SUCCESS: "ADD_TODO_SUCCESS",
   ADD_TODO_ERROR: "ADD_TODO_ERROR",
 
-  // complete and update todo operations
-  COMPLETE_TODO: "COMPLETE_TODO",
-  UPDATE_TODO: "UPDATE_TODO",
+  // complete todo operations
+  COMPLETE_TODO_START: "COMPLETE_TODO_START",
+  COMPLETE_TODO_SUCCESS: "COMPLETE_TODO_SUCCESS",
+  COMPLETE_TODO_ERROR: "COMPLETE_TODO_SUCCESS",
+
+  // update todo operations
+  UPDATE_TODO_START: "UPDATE_TODO_START",
+  UPDATE_TODO_SUCCESS: "UPDATE_TODO_SUCCESS",
+  UPDATE_TODO_ERROR: "UPDATE_TODO_SUCCESS",
 
   // ui operations
   SET_SORT: "SET_SORT",
@@ -33,11 +39,131 @@ export const initialTodoState = {
 
 export function todoReducer(state, action) {
   switch (action.type) {
+    // fetch todo operations
     case TODO_ACTIONS.FETCH_START:
       return {
         ...state,
         isTodoListLoading: true,
         error: "",
+        filterError: "",
+      };
+
+    case TODO_ACTIONS.FETCH_SUCCESS:
+      return {
+        ...state,
+        isTodoListLoading: false,
+        todoList: action.payload,
+      };
+
+    case TODO_ACTIONS.FETCH_ERROR:
+      return {
+        ...state,
+        isTodoListLoading: false,
+        error: action.payload.message,
+      };
+
+    // add todo operations
+    case TODO_ACTIONS.ADD_TODO_START:
+      return {
+        ...state,
+        todoList: [action.payload, ...state.todoList],
+        error: "",
+      };
+
+    case TODO_ACTIONS.ADD_TODO_SUCCESS:
+      return {
+        ...state,
+        todoList: state.todoList.map((todo) =>
+          todo.id === action.payload.tempId ? action.payload.data : todo,
+        ),
+      };
+
+    case TODO_ACTIONS.ADD_TODO_ERROR:
+      return {
+        ...state,
+        error: action.payload.error,
+        todoList: state.todoList.filter(
+          (todo) => todo.id !== action.payload.tempId,
+        ),
+      };
+
+    // complete todo operations
+    case TODO_ACTIONS.COMPLETE_TODO_START:
+      return {
+        ...state,
+        error: "",
+        todoList: action.payload.completedTodos,
+      };
+
+    case TODO_ACTIONS.COMPLETE_TODO_SUCCESS:
+      return {
+        ...state,
+        dataVersion: state.dataVersion + 1,
+      };
+
+    case TODO_ACTIONS.COMPLETE_TODO_ERROR:
+      return {
+        ...state,
+        todoList: state.todoList.map((todo) =>
+          todo.id === action.payload.rollbackTodo.id
+            ? { ...action.payload.rollbackTodo }
+            : todo,
+        ),
+        error: action.payload.message,
+      };
+
+    // update todo operations
+    case TODO_ACTIONS.UPDATE_TODO_START:
+      return {
+        ...state,
+        error: "",
+        todoList: action.payload.updatedTodos,
+      };
+
+    case TODO_ACTIONS.UPDATE_TODO_SUCCESS:
+      return {
+        ...state,
+        dataVersion: state.dataVersion + 1,
+      };
+
+    case TODO_ACTIONS.UPDATE_TODO_ERROR:
+      return {
+        ...state,
+        error: action.payload.message,
+        todoList: state.todoList.map((todo) =>
+          todo.id === action.payload.rollbackTodo.id
+            ? { ...action.payload.rollbackTodo }
+            : todo,
+        ),
+      };
+
+    // ui operations
+    case TODO_ACTIONS.SET_SORT:
+      return {
+        ...state,
+        sortBy: action.payload.sortBy,
+        sortDirection: action.payload.sortDirection,
+      };
+
+    case TODO_ACTIONS.SET_FILTER:
+      return {
+        ...state,
+        filterTerm: action.payload.filter,
+      };
+
+    case TODO_ACTIONS.CLEAR_ERROR:
+      return {
+        ...state,
+        error: "",
+        filterError: "",
+      };
+
+    case TODO_ACTIONS.RESET_FILTERS:
+      return {
+        ...state,
+        sortBy: "createAt",
+        sortDirection: "desc",
+        filterTerm: "",
         filterError: "",
       };
 
